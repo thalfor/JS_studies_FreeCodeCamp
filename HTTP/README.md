dfbda356ef01405b9a6a3fb157b2e36d0a826765
thalfor <thales.fornazari@gmail.com>
2023-01-23 10:08:13 -0300

    Added description

---
+++ HTTP/README.md
@@ -0,0 +1,3 @@
+## Studies about HTTP in JS
+
+In this material we have the basics about creating a LocalHost server using HTTP with a small db that contains Name and Email.
--- HTTP/http.js
+++ HTTP/http.js
@@ -2,28 +2,27 @@ const http = require("http");
 
 const port = 8080;
 
-const server = http.createServer((req, res) =>{
-    if (req.url === "/home"){
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end("<h1>home page</h1>");
-    }
+const server = http.createServer((req, res) => {
+  if (req.url === "/home") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<h1>home page</h1>");
+  }
 
-    if (req.url === '/users'){
-        const users = [
-            {
-                name: 'joao',
-                email: 'joao@joao.joao',
-            },
-            {
-                name: 'joana',
-                email: 'joana@joana.joana',
-            },
-        ];
+  if (req.url === "/users") {
+    const users = [
+      {
+        name: "joao",
+        email: "joao@email.com",
+      },
+      {
+        name: "joana",
+        email: "joana@email.com",
+      },
+    ];
 
-        res.writeHead(200, {"Content-Type": "application/json"});
-        res.end(JSON.stringify(users));
-    }
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(users));
+  }
 });
 
-server.listen(port, ()=> console.log(`rodando na porta: ${port}`));
-
+server.listen(port, () => console.log(`rodando na porta: ${port}`));
