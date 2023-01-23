9d40d83303534ee58af61e9a8e2ec63013537aac
thalfor <thales.fornazari@gmail.com>
2023-01-23 11:03:59 -0300

    code clean

--- phoneValidator/phoneValidator.js
+++ phoneValidator/phoneValidator.js
@@ -12,73 +12,41 @@ formatos aceitos
 1 555-555-5555
 */
 //
-/*
 // função pra pegar o elemento na posição N de um array (se for preciso ta na mão)
-let nthElement = (arr, n = 0) => (n > 0 ? arr.slice(n, n + 1) : arr.slice(n))[0];
+// let nthElement = (arr, n = 0) => (n > 0 ? arr.slice(n, n + 1) : arr.slice(n))[0];
 //
 //
-const telefoneInformado = '757 622-7382';
-const telefoneString = telefoneInformado.toString();
-let telefoneArray = [...telefoneString];
-//
-function containsSpecialChars(str) {
-    const specialChars = /[`!@#$%^&*_+\=\[\]{};':"\\|,.<>\/?~a-z]/;
-    return specialChars.test(str);
-  }
-//
-if (telefoneString.replace(/[^0-9]/g,'').length === 10 || telefoneString.replace(/[^0-9]/g,'').length === 11) {
-    if(containsSpecialChars(telefoneInformado)) {
-        console.log("Telefone inválido 1");
-    } else if (telefoneString.replace(/[^0-9]/g,'').length === 11 && nthElement(telefoneArray,0) != 1) {
-        console.log("Telefone inválido 2");
-    } else if (telefoneString.replace(/[^0-9]/g,'').length === 11 && nthElement(telefoneArray,0) === 1) {
-        if (telefoneString.replace(/[^(]/g,'') === '(' && telefoneString.replace(/[^)]/g,'') === ''){
-            console.log("Telefone inválido 3");
-        } else if (telefoneString.replace(/[^)]/g,'') === ')' && telefoneString.replace(/[^(]/g,'') === ''){
-            console.log("Telefone inválido 4");
-        }
-    }
-    else {
-        console.log("Telefone inválido end");
-    }
-}
-//
-console.log(telefoneString.replace(/[^(]/g,''));
-console.log(telefoneString.replace(/[^)]/g,''));
-//console.log(nthElement(telefoneArray,0));
-//console.log(nthElement(telefoneArray,2));
-//console.log(nthElement(telefoneArray,6));
-//
-*/
-//
-//
-const telefoneInformado = '555-5555';
+const telefoneInformado = "555-5555";
 const telefoneString = telefoneInformado.toString();
 const telefoneArray = [...telefoneString];
-let nthElement = (arr, n = 0) => (n > 0 ? arr.slice(n, n + 1) : arr.slice(n))[0];
+let nthElement = (arr, n = 0) =>
+  (n > 0 ? arr.slice(n, n + 1) : arr.slice(n))[0];
 //
-if (telefoneString.replace(/[^0-9]/g,'').length === 10){
-    if (nthElement(telefoneArray,0) != '(') {
-        let re2 = /^(\d{3})[- ]?(\d{3})[- ]?(\d{4})$/;
-        console.log(re2.test(telefoneString));
-    } else {
-        let re1 = /^\((\d{3})\)[- ]?(\d{3})[- ]?(\d{4})$/;
-        console.log(re1.test(telefoneString));
-    }
-} else if (telefoneString.replace(/[^0-9]/g,'').length === 11 && nthElement(telefoneArray,0) === '1') {
-    let telefoneString2 = telefoneString.slice(1);
-    if (nthElement([...telefoneString2],0) === ' '){
-        telefoneString2 = telefoneString2.slice(1);
-    }
-    if (nthElement([...telefoneString2],0) != '(') {
-        let re2 = /^(\d{3})[- ]?(\d{3})[- ]?(\d{4})$/;
-        console.log(re2.test(telefoneString2));
-    } else {
-        let re1 = /^\((\d{3})\)[- ]?(\d{3})[- ]?(\d{4})$/;
-        console.log(re1.test(telefoneString2));
-    }
+if (telefoneString.replace(/[^0-9]/g, "").length === 10) {
+  if (nthElement(telefoneArray, 0) != "(") {
+    let re2 = /^(\d{3})[- ]?(\d{3})[- ]?(\d{4})$/;
+    console.log(re2.test(telefoneString));
+  } else {
+    let re1 = /^\((\d{3})\)[- ]?(\d{3})[- ]?(\d{4})$/;
+    console.log(re1.test(telefoneString));
+  }
+} else if (
+  telefoneString.replace(/[^0-9]/g, "").length === 11 &&
+  nthElement(telefoneArray, 0) === "1"
+) {
+  let telefoneString2 = telefoneString.slice(1);
+  if (nthElement([...telefoneString2], 0) === " ") {
+    telefoneString2 = telefoneString2.slice(1);
+  }
+  if (nthElement([...telefoneString2], 0) != "(") {
+    let re2 = /^(\d{3})[- ]?(\d{3})[- ]?(\d{4})$/;
+    console.log(re2.test(telefoneString2));
+  } else {
+    let re1 = /^\((\d{3})\)[- ]?(\d{3})[- ]?(\d{4})$/;
+    console.log(re1.test(telefoneString2));
+  }
 } else {
-    console.log('erroooooou');
+  console.log("erroooooou");
 }
 //
 //console.log(telefoneString.replace(/[^0-9]/g,'').length);
